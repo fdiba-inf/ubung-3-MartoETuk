7862200939852cef4aa47a77580deaf6977cba06
MartoETuk <martin_shishkovski@abv.bg>
2020-10-18 10:02:43 +0000

    one more time

--- src/exercise3/Triangle.java
+++ src/exercise3/Triangle.java
@@ -2,39 +2,38 @@ package exercise3;
 
 import java.util.Scanner;
 
-public class Triangle {
+ public class Triangle {
 
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+      double a= input.nextDouble();
+      double b= input.nextDouble();
+      double c = input.nextDouble();
+      String typ = "";
+      do{
+        if (a>0 && b>0 && c>0 && a<(c+b) && b<(a+c) && c< (b+a)){
+          double s = (a + b + c)/2;
+          double area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+          System.out.println("Area: "+area);
+        if(a!=b && b!=c && c!=a){
+         typ = "scalene";
+          System.out.println("Triangle: " + typ);
 
-    double a = input.nextDouble();
-    double b = input.nextDouble();
-    double c = input.nextDouble();
-
-    do {
-      
-      if (a>0 && b>0 && c>0 && a<(b+c) && b<(a+c) && c<(b+a)) {
-        double s = (a+b+c)/2;
-        double area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
-        String type;
-
-        if (a == b && b == c) {
-          type = "equilateral";
-        } else if (a != b && b != c && c != a) {
-          type = "scalene";
-        } else {
-          type = "isosceles";
+        }else if (a==b && b==c){
+          typ="equilateral";
+          System.out.println("Triangle: " + typ);
+        }else if (a==b|| b==c || c==a ){
+          typ= "isosceles";
+          System.out.println("Triangle: " + typ);
+        
         }
-
-        System.out.println("Area: " + area);
-        System.out.println("Triangle: " + type);
-      } else {
+        
+      }else{ 
         System.out.println("Values are not correct!");
-      } 
-
-      a = input.nextDouble();
-      b = input.nextDouble();
+      }
+       a= input.nextDouble();
+      b= input.nextDouble();
       c = input.nextDouble();
-    } while (a <= 0 || b <= 0 || c <= 0);
-  }
-}
+      } while (a>0 && b>0 && c>0);
+    }
+ }
